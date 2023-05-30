ce9c7389035370491ee81edb3735c654d1346588
xuehaoming0527 <2060221756@qq.com>
2023-05-30 21:28:19 +0800

    别名联想设置

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    // 实际的路径转化 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
