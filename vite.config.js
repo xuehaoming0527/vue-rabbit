790c9bd44d5dee5f881cd16e0ce01c740adf955a
xuehaoming0527 <2060221756@qq.com>
2023-05-31 17:07:17 +0800

    elementPlus主题色定制

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,9 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" })],
     }),
   ],
   resolve: {
@@ -25,5 +27,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  },
 })
